bf5b467f3fde4e91697a6fbb41e99a8b286497ad
gaiahead <gaiahead@users.noreply.github.com>
2026-09-06 07:00:54 +0900

    data: hermes cron update 2026-09-06 07:00 KST

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DATA_VERSION = 'data-20260905-score-number';
+const DATA_VERSION = 'data-20260906-0700';
 const PRICE_CHART_TRADING_DAYS = 120;
 const GRID = '#e2e8f0';
 const TICK = '#64748b';
